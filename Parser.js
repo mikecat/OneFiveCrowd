c26a3c7ab9e968b7fc5dfa78e3da1ffbd8d28faf
MikeCAT <mikecat@users.noreply.github.com>
2019-07-27 11:52:25 +0900

    move variableIndice

--- Parser.js
+++ Parser.js
@@ -244,6 +244,13 @@ function_arguments ::= (空)
                      | number | variable | label | string
 */
 
+const variableIndice = {
+	"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6,
+	"H": 7, "I": 8, "J": 9, "K":10, "L":11, "M":12, "N":13,
+	"O":14, "P":15, "Q":16, "R":17, "S":18, "T":19, "U":20,
+	"V":21, "W":22, "X":23, "Y":24, "Z":25
+};
+
 const printModifiers = {
 	"CHR$": null,
 	"DEC$": null,
@@ -329,13 +336,6 @@ const basicFunctions = {
 	"POINT" : null,
 	"COS"   : null,
 	"SIN"   : null
-};
-
-const variableIndice = {
-	"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6,
-	"H": 7, "I": 8, "J": 9, "K":10, "L":11, "M":12, "N":13,
-	"O":14, "P":15, "Q":16, "R":17, "S":18, "T":19, "U":20,
-	"V":21, "W":22, "X":23, "Y":24, "Z":25
 };
 
 var parser = (function() {
