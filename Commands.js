1fddde7b76f28997eb89f94eebdc8e2e345644b9
MikeCAT <mikecat@users.noreply.github.com>
2022-12-27 08:04:29 +0900

    add commands GOTO and END

--- Commands.js
+++ Commands.js
@@ -13,3 +13,18 @@ function commandRUN() {
 	}
 	return [lineToExecute, 0];
 }
+
+function commandGOTO(args) {
+	// プログラムを指定の行から実行する
+	if (prgDirty) compileProgram();
+	if (args[0] > 0 && (args[0] in programs)) {
+		return [args[0], 0];
+	} else {
+		throw "Line error";
+	}
+}
+
+function commandEND(){
+	// プログラムの実行を終了する
+	return [-1, 0];
+}
