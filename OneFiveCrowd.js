49f8421f30c35bd9d0b2d34aabad100b0f19b83d
MikeCAT <mikecat@users.noreply.github.com>
2022-12-27 08:20:48 +0900

    remove extra commandRUN() function

--- OneFiveCrowd.js
+++ OneFiveCrowd.js
@@ -1016,17 +1016,3 @@ function commandCLP() {
 	}
 	fontDirty = true;
 }
-
-function commandRUN() {
-	// プログラムを最初の行から実行する
-	if (prgDirty) compileProgram();
-	let lineToExecute = -1;
-	const keys = Object.keys(programs);
-	for (let i = 0; i < keys.length; i++) {
-		const lineNo = parseInt(keys[i]);
-		if (!isNaN(lineNo) && lineNo > 0 && (lineToExecute <= 0 || lineNo < lineToExecute)) {
-			lineToExecute = lineNo;
-		}
-	}
-	return [lineToExecute, 0];
-}
