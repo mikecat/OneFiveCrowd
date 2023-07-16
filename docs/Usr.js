6b97ca12bf5e3812f0821374fcbd86818ca4bda7
MikeCAT <mikecat@users.noreply.github.com>
2023-07-17 04:23:41 +0900

    fix RV32C memory write

--- docs/Usr.js
+++ docs/Usr.js
@@ -1147,7 +1147,7 @@ async function functionUSR_RV32C(startVirtualAddress, startArgument) {
 	const writeMemory = function(address, size, value) {
 		if (RAM_START_ADDRESS <= address && address + size <= RAM_START_ADDRESS + ramBytes.length) {
 			for (let i = 0; i < size; i++) {
-				writePhysicalRam(address - RAM_START_ADDRESS + i + VIRTUAL_RAM_OFFSET, (value >> (8 * i)) & 0xff);
+				writePhysicalRam(address - RAM_START_ADDRESS + i, (value >> (8 * i)) & 0xff);
 			}
 		} else {
 			segFault("invalid memory write 0x" + address.toString(16));
